aaaa2affd21f517d65531b1308d0e5e1c0fa41b7
Canyon <379657505@qq.com>
2017-01-11 19:01:58 +0800

    添加Hero_Prefab,Effect_Prefab

---
+++ Assets/PackResources/Arts/TestEffect/Shader/FX_Diffuse.shader
@@ -0,0 +1,59 @@
+﻿Shader "YuShen/FX Diffuse"
+ {
+	Properties {
+		_MainTex ("Base (RGBA)", 2D) = "white" {}
+		_DiffuseColor("Diffuse Color",Color ) = (1,1,1,1)
+	}
+	SubShader {
+		Tags {"Queue"="Transparent" "IgnoreProjector"="True" }
+
+		Fog {Mode off}
+		//Blend SrcAlpha OneMinusSrcAlpha
+		Pass 
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			uniform float4 _DiffuseColor;
+			
+			struct appdata
+			{
+			    float4 vertex    : POSITION;
+				float4 color	 : COLOR;
+			    float2 texcoord  : TEXCOORD0;
+			    
+			};
+	
+			struct v2f
+			{
+			    float4 pos 		: SV_POSITION;
+			    half2  tex1 	: TEXCOORD0;
+				half4  color	: TEXCOORD1;
+			};
+
+			v2f vert(appdata v)
+			{
+			    v2f o;
+			    o.pos       = mul( UNITY_MATRIX_MVP, v.vertex );
+			    o.tex1.xy   = v.texcoord.xy;
+			    o.color		= v.color;
+			    return o;
+			}
+	
+			sampler2D _MainTex;
+			
+			fixed4 frag( v2f ps ) : COLOR
+			{
+			   fixed4 cBase = tex2D( _MainTex , ps.tex1 );
+			   fixed4 vColor;
+		       vColor.rgba = cBase * _DiffuseColor  * ps.color;
+	   	 	   return vColor;
+			}
+			
+			ENDCG
+		}
+	  }
+	  FallBack "Diffuse"
+} 
---
+++ Assets/Script/Utility/Component/SpriteAniCurve.cs
@@ -0,0 +1,76 @@
+﻿//***************************************************************
+// 类名：精灵位移曲线
+// 作者：钟汶洁
+// 日期：2016.12
+// 功能：跟随动作的播放的时间，通过预先编辑的位移曲线，使用角色控制器来控制精灵的位移
+//***************************************************************
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 精灵动作曲线
+/// </summary>
+public class SpriteAniCurve : StateMachineBehaviour
+{
+    /// <summary>
+    /// x方向曲线
+    /// </summary>
+    public AnimationCurve x;
+    /// <summary>
+    /// y方向曲线
+    /// </summary>
+    public AnimationCurve y;
+    /// <summary>
+    /// z方向曲线
+    /// </summary>
+    public AnimationCurve z;
+
+    /// <summary>
+    /// 精灵角色控制器
+    /// </summary>
+    public CharacterController charController
+    {
+        get
+        {
+            return m_charController;
+        }
+        set
+        {
+            m_charController = value;
+            if (m_charController != null)
+            {
+                m_transform = m_charController.transform;
+            }
+        }
+    }
+
+    private CharacterController m_charController;
+    private Transform m_transform;
+    private float m_lastNormalizedTime;
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        int a;
+        if (stateInfo.normalizedTime > 0)
+        {
+            a = Mathf.FloorToInt(stateInfo.normalizedTime);
+        }
+        else
+        {
+            a = Mathf.CeilToInt(stateInfo.normalizedTime);
+        }
+        float t = stateInfo.normalizedTime - a;
+  
+        if (this.charController != null)
+        {
+            Vector3 move = Vector3.zero;
+            move.x = x.Evaluate(t * x.length);
+            move.y = y.Evaluate(t * y.length);
+            move.z = z.Evaluate(t * z.length);
+            move = m_transform.TransformVector(move);
+            m_charController.Move(move);
+        }
+    }
+}
+
