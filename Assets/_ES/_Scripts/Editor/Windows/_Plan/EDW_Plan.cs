125449b9c9d94aa835f65a03dc334132c1c25bf2
Canyon <379657505@qq.com>
2017-01-19 16:50:45 +0800

    策划视口窗体，目前读取Buff，写入Buff

---
+++ Assets/_ES/_Scripts/Editor/Windows/_Plan/EDW_Plan.cs
@@ -0,0 +1,323 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+/// <summary>
+/// 类名 : excel 数据编辑器窗口Class
+/// 作者 : Canyon
+/// 日期 : 2017-01-19 14:30
+/// 功能 : 
+/// </summary>
+public class EDW_Plan : EditorWindow {
+	
+	static bool isOpenWindowView = false;
+
+	static protected EDW_Plan vwWindow = null;
+
+	// 窗体宽高
+	static public float width = 600;
+	static public float height = 400;
+
+	[MenuItem("Tools/Windows/ExcelReadWrite")]
+	static void AddWindow()
+	{
+		if (isOpenWindowView || vwWindow != null)
+			return;
+
+		try
+		{
+			isOpenWindowView = true;
+			float x = 460;
+			float y = 200;
+			Rect rect = new Rect(x, y, width, height);
+
+			// 大小不能拉伸
+			// vwWindow = GetWindowWithRect<EDW_Skill>(rect, true, "SkillEditor");
+
+			// 窗口，只能单独当成一个窗口,大小可以拉伸
+			//vwWindow = GetWindow<EDW_Skill>(true,"SkillEditor");
+
+			// 这个合并到其他窗口中去,大小可以拉伸
+			vwWindow = GetWindow<EDW_Plan>("ExcelReadWrite");
+
+			vwWindow.position = rect;
+		}
+		catch (System.Exception)
+		{
+			OnClearSWindow();
+			throw;
+		}
+	}
+
+	static void OnClearSWindow()
+	{
+		isOpenWindowView = false;
+		vwWindow = null;
+	}
+
+	#region  == Member Attribute ===
+
+	Vector2 scrollPos;
+	float topDescH = 0;
+	float botBtnH = 20;
+	float minScrollH = 260;
+	float curScrollH = 260;
+	float minWidth = 440;
+	float curWidth = 0;
+
+	int m_indExcelTable;
+	int _m_preIndexET = 0;
+	string[] ExcelTable = { "Buffer表","子弹表" };
+
+	string pathOpenExcel;
+
+	EG_Buff m_egBuff = new EG_Buff();
+
+	// delegate 更新
+	System.Action call4OnUpdate;
+	System.Action<SceneView> call4OnSceneViewGUI;
+
+	#endregion
+
+	#region  == EditorWindow Func ===
+
+	void Awake()
+	{
+		DoInit();
+	}
+
+	void OnEnable()
+	{
+		EditorApplication.update += OnUpdate;
+		SceneView.onSceneGUIDelegate += OnSceneGUI;
+	}
+
+	void OnDisable()
+	{
+		EditorApplication.update -= OnUpdate;
+		SceneView.onSceneGUIDelegate -= OnSceneGUI;
+	}
+
+	void OnGUI()
+	{
+		EG_GUIHelper.FEG_BeginV ();
+		{
+			EG_GUIHelper.FEG_BeginH ();
+			{
+				GUIStyle style = EditorStyles.label;
+				style.alignment = TextAnchor.MiddleCenter;
+				string txtDecs = "类名 : excel数据表编辑器窗口\n"
+					+ "作者 : Canyon\n"
+					+ "日期 : 2017 - 01 - 19 14:50\n"
+					+ "描述 : 目前可以操作Buff表。\n";
+				GUILayout.Label(txtDecs, style);
+				style.alignment = TextAnchor.MiddleLeft;
+			}
+			EG_GUIHelper.FEG_EndH ();
+
+			EG_GUIHelper.FEG_BeginH ();
+			m_indExcelTable = EditorGUILayout.Popup ("选择表类型:", m_indExcelTable, ExcelTable);
+			if (_m_preIndexET != m_indExcelTable) {
+				DoClear ();
+			}
+			EG_GUIHelper.FEG_EndH ();
+
+			RecokenWH ();
+
+			EG_GUIHelper.FG_Space (5);
+
+			EG_GUIHelper.FEG_BeginScroll(ref scrollPos,0,curWidth,curScrollH);
+			_DrawSearchExcel();
+
+			switch (m_indExcelTable) {
+			case 0:
+				m_egBuff.DrawExcel ();
+				break;
+			}
+
+			EG_GUIHelper.FEG_EndScroll();
+
+			_DrawOptExcelBtns();
+		}
+		EG_GUIHelper.FEG_EndV ();
+	}
+
+	// 在给定检视面板每秒10帧更新
+	void OnInspectorUpdate()
+	{
+		Repaint();
+	}
+
+	void OnDestroy()
+	{
+		OnClearSWindow();
+		EditorApplication.update -= OnUpdate;
+		SceneView.onSceneGUIDelegate -= OnSceneGUI;
+
+		DoClear();
+	}
+
+	#endregion
+
+	#region  == Self Func ===
+
+	void DoInit()
+	{
+		
+	}
+
+	void OnUpdate()
+	{
+		if (this.call4OnUpdate != null)
+		{
+			this.call4OnUpdate();
+		}
+	}
+
+	void DoClear()
+	{
+		m_egBuff.DoClear ();
+	}
+
+	void OnSceneGUI(SceneView sceneView) {
+		// Do your drawing here using Handles.
+		Handles.BeginGUI();
+		// Do your drawing here using GUI.
+		Handles.EndGUI();    
+
+		if (this.call4OnSceneViewGUI != null) {
+			this.call4OnSceneViewGUI (sceneView);
+		}
+	}
+
+	public void AddCall4Update(System.Action callFunc)
+	{
+		if(this.call4OnUpdate == null)
+		{
+			this.call4OnUpdate = callFunc;
+		}else
+		{
+			this.call4OnUpdate += callFunc;
+		}
+	}
+
+	public void RemoveCall4Update(System.Action callFunc)
+	{
+		if (this.call4OnUpdate != null)
+		{
+			this.call4OnUpdate -= callFunc;
+		}
+	}
+
+	public void AddCall4SceneGUI(System.Action<SceneView> callFunc)
+	{
+		if(this.call4OnSceneViewGUI == null)
+		{
+			this.call4OnSceneViewGUI = callFunc;
+		}else
+		{
+			this.call4OnSceneViewGUI += callFunc;
+		}
+	}
+
+	public void RemoveCall4Update(System.Action<SceneView> callFunc)
+	{
+		if (this.call4OnSceneViewGUI != null)
+		{
+			this.call4OnSceneViewGUI -= callFunc;
+		}
+	}
+
+	void RecokenWH()
+	{
+		// 100 - 是主界面顶部高度 20 - 是误差偏移
+		curScrollH = (this.position.height - 100) - topDescH - botBtnH;
+		curScrollH = Mathf.Max(curScrollH, minScrollH);
+
+		curWidth = (this.position.width);
+		curWidth = Mathf.Max(curWidth, minWidth);
+	}
+
+	bool isReady(){
+		switch (m_indExcelTable) {
+		case 0:
+			return m_egBuff.isInited;
+		}
+		return false;
+	}
+
+	void SaveExcel(string savePath){
+		switch (m_indExcelTable) {
+		case 0:
+			m_egBuff.SaveExcel (savePath);
+			break;
+		}
+	}
+
+	#endregion
+
+	#region == Draw Func ==
+
+	void _DrawSearchExcel()
+	{
+		EG_GUIHelper.FEG_BeginH();
+		if (GUILayout.Button("选取Excel表"))
+		{
+			this.pathOpenExcel = UnityEditor.EditorUtility.OpenFilePanel("选取excel文件", "", "xls");
+			switch (m_indExcelTable) {
+			case 0:
+				m_egBuff.DoInit(this.pathOpenExcel);
+				break;
+			}
+		}
+		EG_GUIHelper.FEG_EndH();
+
+		EG_GUIHelper.FG_Space(5);
+
+		if (string.IsNullOrEmpty(this.pathOpenExcel))
+		{
+			EG_GUIHelper.FEG_BeginH();
+			EditorGUILayout.HelpBox("请单击选取Excel,目前不能根据ID进行查询?", MessageType.Error);
+			EG_GUIHelper.FEG_EndH();
+		}else
+		{
+			EG_GUIHelper.FEG_BeginH();
+			EG_GUIHelper.FG_Label("SkillPath:" + this.pathOpenExcel);
+			EG_GUIHelper.FEG_EndH();
+		}
+
+		EG_GUIHelper.FG_Space(5);
+	}
+
+	void _DrawOptExcelBtns()
+	{
+		EG_GUIHelper.FEG_BeginH();
+		{
+			if (GUILayout.Button("Save Excel"))
+			{
+				if (!isReady())
+				{
+					EditorUtility.DisplayDialog("Tips", "没有选则表，不能进行保存!", "Okey");
+					return;
+				}
+
+				string folder = Path.GetDirectoryName(this.pathOpenExcel);
+				string fileName = Path.GetFileNameWithoutExtension(this.pathOpenExcel);
+				string suffix = Path.GetExtension(this.pathOpenExcel);
+				suffix = suffix.Replace(".", "");
+				string savePath = UnityEditor.EditorUtility.SaveFilePanel("保存Excel文件", folder, fileName, suffix);
+
+				if (string.IsNullOrEmpty(savePath))
+				{
+					UnityEditor.EditorUtility.DisplayDialog("Tips", "The save path is Empty !!!", "Okey");
+					return;
+				}
+
+				SaveExcel (savePath);
+			}
+		}
+		EG_GUIHelper.FEG_EndH();
+	}
+	#endregion
+}
---
+++ Assets/_ES/_Scripts/Editor/Windows/_Xls/EG_Buff.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// 类名 : buff 在 windows的视图
+/// 作者 : Canyon
+/// 日期 : 2017-01-19 17:10
+/// 功能 : 
+/// </summary>
+public class EG_Buff {
+
+	EN_Buff ms_curEnity;
+
+	int ms_ID;
+	string ms_name;
+	string ms_desc;
+	int ms_nameId;
+	int ms_descId;
+	string ms_icon;
+	GameObject ms_gobjEffect;
+	string ms_res = "";
+	int ms_join;
+	string[] JoinType = {
+		"原点",
+		"头部",
+		"胸部",
+		"腰部",
+		"左手心",
+		"右手心",
+		"左武器攻击点",
+		"右武器攻击点"
+	};
+	string ms_mate;
+	int ms_tag;
+	string[] TagType = {
+		"增益",
+		"减益"
+	};
+	int ms_gid;
+	string[] GIDType = {
+		"占位"
+	};
+	bool ms_isRest;
+	string ms_sInv;
+	string ms_sOne;
+	float ms_fInv;
+	string ms_sDur;
+
+	EN_BuffOpt optBuff{
+		get{
+			return EN_BuffOpt.Instance;
+		}
+	}
+
+	public void DoInit(string path){
+		optBuff.DoInit (path, 0);
+	}
+
+	public bool isInited{
+		get{
+			return optBuff.isInitSuccessed;
+		}
+	}
+
+	public void DoClear(){
+		optBuff.DoClear ();
+	}
+
+	public void DrawExcel()
+	{
+		EG_GUIHelper.FEG_BeginH();
+		ms_ID = EditorGUILayout.IntField("ID:",ms_ID);
+		if (GUILayout.Button("查询"))
+		{
+			if (optBuff.isInitSuccessed)
+			{
+				ms_curEnity = optBuff.GetEntity(ms_ID);
+				OnInitEntity2Attrs();
+			}
+			else{
+				EditorUtility.DisplayDialog("Tips","没有选则技能表，不能进行查询搜索!","Okey");
+			}
+		}
+		EG_GUIHelper.FEG_EndH();
+		EG_GUIHelper.FG_Space(5);
+
+		ms_name = EditorGUILayout.TextField("名称:", ms_name);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_nameId = EditorGUILayout.IntField("名称ID:", ms_nameId);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_desc = EditorGUILayout.TextField("描述:", ms_desc);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_descId = EditorGUILayout.IntField("描述ID:", ms_descId);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_icon = EditorGUILayout.TextField("图标Icon:", ms_icon);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_gobjEffect = EditorGUILayout.ObjectField ("资源:", ms_gobjEffect, typeof(GameObject), false) as GameObject;
+		EG_GUIHelper.FG_Space(5);
+
+		EditorGUILayout.LabelField ("名称:" + ms_res);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_join = EditorGUILayout.Popup("挂节点:", ms_join, JoinType);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_mate = EditorGUILayout.TextField("材质变化:", ms_mate);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_tag = EditorGUILayout.Popup("主类型:", ms_tag, TagType);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_gid = EditorGUILayout.Popup("子类型:", ms_gid, GIDType);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_isRest = EditorGUILayout.Toggle("替代是否重置:", ms_isRest);
+		EG_GUIHelper.FG_Space(5);
+
+		EditorGUILayout.LabelField ("持续效果:", ms_sInv);
+		EG_GUIHelper.FG_Space(5);
+
+		EditorGUILayout.LabelField ("瞬时效果:", ms_sInv);
+		EG_GUIHelper.FG_Space(5);
+
+		ms_fInv = EditorGUILayout.FloatField ("持续效果的间隔时间(单位秒):", ms_fInv);
+		EG_GUIHelper.FG_Space(5);
+
+		EditorGUILayout.LabelField ("特殊效果:", ms_sDur);
+		EG_GUIHelper.FG_Space(5);
+	}
+
+	void OnInitEntity2Attrs()
+	{
+		if(ms_curEnity != null)
+		{
+			this.ms_ID = this.ms_curEnity.ID;
+			this.ms_name = this.ms_curEnity.Name;
+			this.ms_nameId = this.ms_curEnity.NameID;
+			this.ms_desc = this.ms_curEnity.Desc;
+			this.ms_descId = this.ms_curEnity.DescID;
+
+			this.ms_icon = this.ms_curEnity.Icon;
+
+			this.ms_res = this.ms_curEnity.EffectResName;
+			this.ms_join = this.ms_curEnity.JoinId;
+			this.ms_mate = this.ms_curEnity.MateChange;
+			this.ms_tag = this.ms_curEnity.Tag;
+			this.ms_gid = this.ms_curEnity.GID;
+			this.ms_isRest = this.ms_curEnity.IsResetWhenGet == 1 ? true : false;
+			this.ms_sInv = this.ms_curEnity.strEvtInterval;
+			this.ms_sOne = this.ms_curEnity.strEvtOnce;
+			this.ms_fInv = this.ms_curEnity.Interval;
+			this.ms_sDur = this.ms_curEnity.strEvtDuration;
+		}
+	}
+
+	void OnInitAttrs2Entity()
+	{
+		ms_curEnity = optBuff.GetOrNew(ms_ID);
+		this.ms_curEnity.ID = this.ms_ID;
+		this.ms_curEnity.Name = this.ms_name;
+		this.ms_curEnity.NameID = this.ms_nameId;
+		this.ms_curEnity.Desc = this.ms_desc;
+		this.ms_curEnity.DescID = this.ms_descId;
+
+		this.ms_curEnity.Icon = this.ms_icon;
+		this.ms_curEnity.EffectResName = this.ms_res;
+		this.ms_curEnity.JoinId = this.ms_join;
+		this.ms_curEnity.MateChange = this.ms_mate;
+		this.ms_curEnity.Tag = this.ms_tag;
+		this.ms_curEnity.GID = this.ms_gid;
+		this.ms_curEnity.IsResetWhenGet = this.ms_isRest ? 1 : 0;
+		this.ms_curEnity.strEvtInterval = this.ms_sInv;
+		this.ms_curEnity.strEvtOnce = this.ms_sOne;
+		this.ms_curEnity.Interval = this.ms_fInv;
+		this.ms_curEnity.strEvtDuration = this.ms_sDur;
+	}
+
+	public void SaveExcel(string savePath){
+		OnInitAttrs2Entity ();
+		optBuff.Save (savePath);
+	}
+}
