7795f2c31af820e18bbda07a5eb0475bdaf11ffe
Canyon <379657505@qq.com>
2017-01-19 14:29:47 +0800

    添加excel的buff表结构和操作

---
+++ Assets/_ES/_Scripts/Data/Bean/EN_Buff.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : excel Buff 实体对象
+/// 作者 : Canyon
+/// 日期 : 2017-01-19 11:10
+/// 功能 : 
+/// </summary>
+public class EN_Buff : EN_BaseXls{
+	// public int ID;
+	public string Name;
+
+	public string Desc;
+	// 语言表里面的ID
+	public int NameID;
+	// 语言表里面的ID
+	public int DescID;
+
+	public string Icon;
+	public string EffectResName;
+	public int JoinId;
+	public string MateChange;
+	public int Tag;
+	public int GID;
+	public int IsResetWhenGet;
+
+	public string strEvtInterval;
+	public string strEvtOnce;
+	public float Interval;
+	public string strEvtDuration;
+
+	public EN_Buff():base(){
+	}
+
+	protected override object[] Columns
+	{
+		get { 
+			object[] ret = {
+				this.ID,
+				this.Name,
+
+				this.Desc,
+				this.NameID,
+				this.DescID,
+
+				this.Icon,
+				this.EffectResName,
+				this.JoinId,
+				this.MateChange,
+				this.Tag,
+				this.GID,
+				this.IsResetWhenGet,
+				this.strEvtInterval,
+				this.strEvtOnce,
+				this.Interval,
+				this.strEvtDuration
+			};
+			return ret;
+		}
+	}
+
+	public override void DoInit (int rowIndex, NH_Sheet sheet)
+	{
+		base.DoInit (rowIndex, sheet);
+
+		int colIndex = 0;
+		this.ID = sheet.GetInt(rowIndex, colIndex++);
+		this.Name = sheet.GetString(rowIndex, colIndex++);
+
+		this.Desc = sheet.GetString(rowIndex, colIndex++);
+		this.NameID = sheet.GetInt(rowIndex, colIndex++);
+		this.DescID = sheet.GetInt(rowIndex, colIndex++);
+
+		this.Icon = sheet.GetString(rowIndex, colIndex++);
+		this.EffectResName = sheet.GetString(rowIndex, colIndex++);
+		this.JoinId = sheet.GetInt(rowIndex, colIndex++);
+		this.MateChange = sheet.GetString(rowIndex, colIndex++);
+		this.Tag = sheet.GetInt(rowIndex, colIndex++);
+		this.GID = sheet.GetInt(rowIndex, colIndex++);
+		this.IsResetWhenGet = sheet.GetInt(rowIndex, colIndex++);
+		this.strEvtInterval = sheet.GetString(rowIndex, colIndex++);
+		this.strEvtOnce = sheet.GetString(rowIndex, colIndex++);
+		this.Interval = sheet.GetFloat(rowIndex, colIndex++);
+		this.strEvtDuration = sheet.GetString(rowIndex, colIndex++);
+	}
+
+	static public EN_Buff NewBuff(int rowIndex, NH_Sheet sheet)
+	{
+		EN_Buff one = new EN_Buff();
+		one.DoInit (rowIndex, sheet);
+		return one;
+	}
+}
---
+++ Assets/_ES/_Scripts/Data/Opt/EN_BuffOpt.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 类名 : Buff实体操作
+/// 作者 : Canyon
+/// 日期 : 2016-12-29 17:10:00
+/// 功能 : 
+/// </summary>
+public class EN_BuffOpt {
+
+	static EN_BuffOpt _instance;
+	static public EN_BuffOpt Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new EN_BuffOpt();
+			}
+			return _instance;
+		}
+	}
+
+	private EN_BuffOpt() { }
+
+	EN_BaseXlsOpt<EN_Buff> m_eOptXls = new EN_BaseXlsOpt<EN_Buff>();
+
+	public bool isInitSuccessed = false;
+
+	public void DoInit(string path, int sheetIndex)
+	{
+		m_eOptXls.DoInit (path, sheetIndex);
+		isInitSuccessed = m_eOptXls.isInitSuccessed;
+	}
+
+	public EN_Buff GetEnSkill(int ID)
+	{
+		return m_eOptXls.GetEntity (ID);
+	}
+
+	public EN_Buff GetOrNew(int ID)
+	{
+		return m_eOptXls.GetOrNew(ID);
+	}
+
+	public void Save(string savePath)
+	{
+		m_eOptXls.Save (savePath);
+	}
+
+}
