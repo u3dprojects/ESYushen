9a1bba9dc5993ee9716568d14825c346bd35acad
Canyon <379657505@qq.com>
2017-01-20 11:30:45 +0800

    枚举备注

---
+++ Assets/_ES/_Scripts/HelpEx/EnumExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.ComponentModel;
+
+/// <summary>
+/// Enum extension.
+/// 枚举扩展对象获取描述信息
+/// </summary>
+public static class EnumExtension {
+	
+	public static string GetRemark(this Enum value)
+	{
+		FieldInfo fi = value.GetType().GetField(value.ToString());
+		if (fi == null)
+		{
+			return value.ToString();
+		}
+		object[] attributes = fi.GetCustomAttributes(typeof(RemarkAttribute), false);
+		if (attributes.Length > 0)
+		{
+			return ((RemarkAttribute)attributes[0]).Remark;
+		}
+		else
+		{
+			return value.ToString();
+		}
+	}
+
+	public static string GetDescription(this Enum value)
+	{
+		FieldInfo fi = value.GetType().GetField(value.ToString());
+		DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+		if (attributes.Length > 0)
+		{
+			return attributes[0].Description;
+		}
+		else
+		{
+			return value.ToString();
+		}
+	}
+}
---
+++ Assets/_ES/_Scripts/HelpEx/RemarkAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// 备注特性
+/// </summary>
+public class RemarkAttribute : Attribute {
+	/// <summary>
+	/// 备注
+	/// </summary>
+	public string Remark{ get; set;}
+
+	public RemarkAttribute(string remark)
+	{
+		this.Remark = remark;
+	}
+}
