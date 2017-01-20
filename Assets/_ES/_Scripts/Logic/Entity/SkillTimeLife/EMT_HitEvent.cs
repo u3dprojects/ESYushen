d6f87c4fd3794dcd06f3979bed360a50fd1f316d
Canyon <379657505@qq.com>
2017-01-20 16:44:16 +0800

    抽出事件父类

---
+++ Assets/_ES/_Scripts/Logic/Entity/SkillTimeLife/EMT_HitEvent.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+
+/// <summary>
+/// 类名 : 命中事件
+/// 作者 : Canyon
+/// 日期 : 2017-01-18 09:20
+/// 功能 : 受击者(被技能伤害的人),受到的状态(伤害，音效等事件)
+/// </summary>
+public class EMT_HitEvent : EMT_TBases {
+
+	// 是否可以显示
+	public bool m_isCanShow = false;
+
+	// 修改属性事件
+	List<EDT_Property> m_lAttrs = new List<EDT_Property>();
+
+	public override int OninitOne (LitJson.JsonData one, float castTime = 0)
+	{
+		int typeInt = base.OninitOne (one, castTime);
+		switch (typeInt) {
+		case 7:
+		case 8:
+			ToChangeAttrs (one,typeInt);
+			break;
+		}
+		return typeInt;
+	}
+
+	void ToChangeAttrs(JsonData jsonData,int tpInt){
+		EDT_Property status = EDT_Property.NewEntity<EDT_Property>(jsonData);
+		if (status != null) {
+			status.m_emTag = (EDT_Property.PropretyTag)(tpInt - 7);
+			m_lEvents.Add (status);
+		}
+	}
+
+	public List<EDT_Property> GetLAttrs(){
+		GetList<EDT_Property> (ref m_lAttrs);
+		return m_lAttrs;
+	}
+
+	public override void DoClear ()
+	{
+		base.DoClear ();
+		m_lAttrs.Clear ();
+	}
+
+	public JsonData ToJsonData(){
+		return ToArrayJsonData(m_lEvents);
+	}
+}
---
+++ Assets/_ES/_Scripts/Logic/Entity/SkillTimeLife/EMT_TBase.cs
@@ -0,0 +1,310 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+
+/// <summary>
+/// 类名 : Event 事件管理 基础类 脚本
+/// 作者 : Canyon
+/// 日期 : 2017-01-20 14:14
+/// 功能 : 管理事件
+/// </summary>
+public class EMT_TBases {
+	
+	// 总和对象
+	protected List<EDT_Base> m_lEvents = new List<EDT_Base>();
+	protected EDT_Base m_tmpEvent;
+	protected int lens = 0;
+
+	// 特效事件
+	List<EDT_Effect> m_lEffects = new List<EDT_Effect>();
+
+	// 伤害事件
+	List<EDT_Hurt> m_lHurts = new List<EDT_Hurt>();
+
+	// 音效事件
+	List<EDT_Audio> m_lAudios = new List<EDT_Audio>();
+
+	// 震屏事件
+	List<EDT_Shake> m_lShakes = new List<EDT_Shake>();
+
+	public T NewEvent<T>() where T : EDT_Base,new()
+	{
+		T ret = EDT_Base.NewEntity<T>();
+		m_lEvents.Add(ret);
+		return ret;
+	}
+
+	public T NewEvent<T>(float castTime,JsonData jsonData) where T : EDT_Base,new()
+	{
+		T ret = EDT_Base.NewEntity<T>(jsonData, castTime);
+		if (ret != null) {
+			m_lEvents.Add (ret);
+		}
+		return ret;
+	}
+
+	public void RmEvent(EDT_Base en)
+	{
+		if (en == null)
+			return;
+		en.DoClear ();
+
+		m_lEvents.Remove (en);
+	}
+
+	protected List<T> GetList<T>() where T : EDT_Base
+	{
+		List<T> ret = new List<T>();
+		lens = m_lEvents.Count;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			if(m_tmpEvent is T)
+			{
+				ret.Add((T)m_tmpEvent);
+			}
+		}
+		return ret;
+	}
+
+	public void GetList<T>(ref List<T> ret) where T : EDT_Base
+	{
+		if (ret == null) {
+			ret = new List<T> ();
+		} else {
+			ret.Clear ();
+		}
+
+		lens = m_lEvents.Count;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			if(m_tmpEvent is T)
+			{
+				ret.Add((T)m_tmpEvent);
+			}
+		}
+	}
+
+	#region === 周期函数 init - start - update - end 等 === 
+
+	public void DoReInit(JsonData jsonData,float castTime = 0){
+		DoClear ();
+		DoInit (jsonData,castTime);
+	}
+
+	public void DoInit (JsonData jsonData,float castTime = 0)
+	{
+		if (jsonData == null || !jsonData.IsArray)
+			return;
+		for (int j = 0; j < jsonData.Count; j++) {
+			OninitOne(jsonData[j],castTime);
+		}
+	}
+
+	public virtual int OninitOne(JsonData one,float castTime = 0){
+		int typeInt = (int)((one) ["m_typeInt"]);
+		switch (typeInt) {
+		case 1:
+			ToEffect(castTime, one);
+			break;
+		case 2:
+			ToAudio(castTime, one);
+			break;
+		case 3:
+			ToShake(castTime, one);
+			break;
+		case 6:
+			ToHurt(castTime, one);
+			break;
+		}
+		return typeInt;
+	}
+
+	public virtual void DoStart()
+	{
+		lens = m_lEvents.Count;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			m_tmpEvent.DoStart(true);
+		}
+		m_tmpEvent = null;
+	}
+
+	public void DoUpdate(float deltatime)
+	{
+		lens = m_lEvents.Count;
+		if (lens <= 0)
+			return;
+
+
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			m_tmpEvent.DoUpdate(deltatime);
+			if (m_tmpEvent.m_isEnd)
+			{
+				m_tmpEvent.DoEnd();
+			}
+		}
+	}
+
+	public virtual void OnSceneGUI(Transform trsfOrg)
+	{
+		lens = m_lEvents.Count;
+		if (lens <= 0)
+			return;
+		
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			m_tmpEvent.DoSceneGUI(trsfOrg);
+		}
+	}
+
+	public void DoEnd()
+	{
+		lens = m_lEvents.Count;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			m_tmpEvent.DoEnd();
+		}
+		m_tmpEvent = null;
+	}
+
+	public virtual void DoClear()
+	{
+		lens = m_lEvents.Count;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			m_tmpEvent.DoClear ();
+		}
+
+		m_lEvents.Clear();
+
+		m_tmpEvent = null;
+
+		m_lEffects.Clear ();
+		m_lHurts.Clear ();
+		m_lAudios.Clear ();
+		m_lShakes.Clear ();
+	}
+
+	#endregion
+
+	#region === 转换 Func === 
+
+	/// <summary>
+	/// 默认转为主动触发事件，有时间轴
+	/// </summary>
+	/// <returns>The string json data.</returns>
+	public virtual string ToJsonString(){
+		lens = m_lEvents.Count;
+		if (lens <= 0) {
+			return "";
+		}
+
+		Dictionary<float,List<EDT_Base>> tmpDic = new Dictionary<float, List<EDT_Base>> ();
+		List<EDT_Base> tmpList = null;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = m_lEvents[i];
+			if (!m_tmpEvent.m_isInitedFab)
+				continue;
+
+			if (tmpDic.ContainsKey (m_tmpEvent.m_fCastTime)) {
+				tmpList = tmpDic [m_tmpEvent.m_fCastTime];
+			} else {
+				tmpList = new List<EDT_Base> ();
+				tmpDic.Add (m_tmpEvent.m_fCastTime, tmpList);
+			}
+
+			tmpList.Add (m_tmpEvent);
+		}
+
+		if (tmpDic.Count <= 0) {
+			return "";
+		}
+
+		JsonData tmpData = new JsonData ();
+		tmpData.SetJsonType (JsonType.Array);
+
+		JsonData tmpData2;
+		foreach (KeyValuePair<float,List<EDT_Base>> item in tmpDic) {
+			tmpData2 = new JsonData ();
+			tmpData2 ["m_timing"] = EDT_Base.Round2D(item.Key,2);
+			tmpData2 ["m_castEvts"] = ToArrayJsonData (item.Value);
+			tmpData.Add (tmpData2);
+		}
+
+		return JsonMapper.ToJson (tmpData);
+	}
+
+	public JsonData ToArrayJsonData(List<EDT_Base> listOrg){
+		if (listOrg == null || listOrg.Count <= 0) {
+			return null;
+		}
+
+		JsonData ret = new JsonData ();
+		ret.SetJsonType (JsonType.Array);
+
+		lens = listOrg.Count;
+		JsonData tmp = null;
+		for (int i = 0; i < lens; i++)
+		{
+			m_tmpEvent = listOrg[i];
+			tmp = m_tmpEvent.ToJsonData();
+			if (tmp != null) {
+				ret.Add (tmp);
+			}
+		}
+		m_tmpEvent = null;
+		return ret;
+	}
+
+	// 转为特效事件
+	protected void ToEffect(float time,JsonData data){
+		NewEvent<EDT_Effect> (time,data);
+	}
+
+	// 转为打击点事件
+	protected void ToHurt(float time,JsonData data){
+		NewEvent<EDT_Hurt> (time,data);
+	}
+
+	// 转为技能声音
+	protected void ToAudio(float time,JsonData data){
+		NewEvent<EDT_Audio> (time,data);
+	}
+
+	// 转为技能震屏
+	protected void ToShake(float time,JsonData data){
+		NewEvent<EDT_Shake> (time,data);
+	}
+
+	public List<EDT_Effect> GetLEffects(){
+		GetList<EDT_Effect> (ref m_lEffects);
+		return m_lEffects;
+	}
+
+	public List<EDT_Hurt> GetLHurts(){
+		GetList<EDT_Hurt> (ref m_lHurts);
+		return m_lHurts;
+	}
+
+	public List<EDT_Audio> GetLAudios(){
+		GetList<EDT_Audio> (ref m_lAudios);
+		return m_lAudios;
+	}
+
+	public List<EDT_Shake> GetLShakes(){
+		GetList<EDT_Shake> (ref m_lShakes);
+		return m_lShakes;
+	}
+
+	#endregion
+}
