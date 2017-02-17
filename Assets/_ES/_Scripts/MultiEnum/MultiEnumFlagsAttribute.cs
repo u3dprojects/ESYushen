68f7add88a9f983bdf6dd2b1a75ebe600e1b62bd
Canyon <379657505@qq.com>
2017-02-17 18:23:48 +0800

    添加可以多选的属性

---
+++ Assets/_ES/_Scripts/MultiEnum/MultiEnumFlagsAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : 枚举多选属性定义
+/// 作者 : Canyon
+/// 日期 : 2017-02-17 17:30
+/// 功能 : 
+/// </summary>
+public class MultiEnumFlagsAttribute : PropertyAttribute {
+}
